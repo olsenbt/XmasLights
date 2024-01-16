4cf442911d71f5fd9bb2251cdd69a92539229d9d
olsenbt72 <olsenbt@uw.edu>
2024-01-16 15:16:22 -0800

    fix js

--- index.js
+++ index.js
@@ -56,3 +56,42 @@ function showPage(pageId) {
       }
   });
 }
+
+// Function to run a script
+function runScript(scriptName) {
+  // Update the apiUrl with the new API endpoint and IP address
+  var apiUrl = `https://bennettolsen.us:5000/${scriptName}`;
+
+  // Send a GET request to the updated API endpoint
+  fetch(apiUrl)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`Error: ${response.status}`);
+          }
+          return response.text();
+      })
+      .then(result => {
+          console.log(result);
+      })
+      .catch(error => {
+          console.error(error);
+          alert(`Error executing ${scriptName} script: ${error.message}`);
+      });
+
+  // Toggle the "selected" class
+  toggleSelected(scriptName);
+}
+
+function toggleSelected(scriptName) {
+  // Get all buttons
+  var buttons = document.getElementsByTagName('button');
+
+  // Remove the "selected" class from all buttons
+  for (var i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove('selected');
+  }
+
+  // Add the "selected" class to the clicked button
+  var clickedButton = document.getElementById(`${scriptName}Button`);
+  clickedButton.classList.add('selected');
+}
