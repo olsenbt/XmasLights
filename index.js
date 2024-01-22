d79f6fb825edb2bbe3a453aa8f4c5124172ace9a
olsenbt72 <olsenbt@uw.edu>
2024-01-21 23:49:30 -0800

    add slider

--- index.js
+++ index.js
@@ -1,124 +1,142 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Check if the user is authenticated, if not, show the login page
-  if (!isAuthenticated()) {
+document.addEventListener('DOMContentLoaded', function () {
+  // Check if user has saved password
+  if (localStorage.getItem('password') == null) {
     showLoginPage();
-    return;
+  } else {
+    showPage('buttonsPage');
   }
 
-  showPage('buttonsPage'); // Show the buttons page by default
+  // Add event listener to login form
+  var loginForm = document.getElementById('login-form');
+  loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var password = document.getElementById('password').value;
+    localStorage.setItem('password', password);
+    //
+    showPage('buttonsPage');
+  });
 
   // Add event listeners to navigation buttons
   var buttons = document.querySelectorAll('#navigation button');
-  buttons.forEach(function(button) {
-      button.addEventListener('click', function() {
-          var pageId = this.getAttribute('data-page');
-          showPage(pageId);
-      });
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      var pageId = this.getAttribute('data-page');
+      showPage(pageId);
+    });
   });
 
   // Initialize the iro.js color picker with color wheel and darkness slider
   var colorPicker = new iro.ColorPicker('#colorControls', {
-      width: 200,
-      borderWidth: 1,
-      borderColor: '#fff',
-      layout: [
-          { component: iro.ui.Wheel },
-          { component: iro.ui.Slider, options: { sliderType: 'value' } }
-      ],
-      color: "#ff0000" // Default color
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#fff',
+    layout: [{
+        component: iro.ui.Wheel
+      },
+      {
+        component: iro.ui.Slider,
+        options: {
+          sliderType: 'value'
+        }
+      }
+    ],
+    color: "#ff0000" // Default color
   });
 
   // Event listener for color changes
-  colorPicker.on('color:change', function(color) {
-      // Update the input field with the selected color in HEX format
-      document.getElementById('colorPicker').value = color.hexString;
+  colorPicker.on('color:change', function (color) {
+    // Update the input field with the selected color in HEX format
+    document.getElementById('colorPicker').value = color.hexString;
+    // Make an API call to set the lights
+    setLights(color.rgb.r, color.rgb.g, color.rgb.b);
   });
 
+  function setLights(r, g, b) {
+    // Make the API call to set the lights to the specified color
+    fetch('/set_lights', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('password'), // Include the authorization header
+      },
+      body: JSON.stringify({ r, g, b }),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(result => {
+      console.log(result); // Log the server response
+    })
+    .catch(error => {
+      console.error(`Error setting lights: ${error.message}`);
+    });
+  }
+
   // Event listener for darkness slider changes
-  colorPicker.on('input:change', function(color) {
+  colorPicker.on('input:change', function (color) {
+    // Check if color object is defined
+    if (color && color._color && typeof color._color.v !== 'undefined') {
       // Synchronize the darkness slider with the color wheel
       var brightness = 1 - color._color.v; // Invert the darkness value
-      colorPicker.color.set({ v: brightness }); // Set the brightness value
+      colorPicker.color.set({
+        v: brightness
+      }); // Set the brightness value
+    }
   });
 });
 
 function showPage(pageId) {
-  // Hide all pages
-  var pages = document.querySelectorAll('[id$="Page"]'); // Select all elements with IDs ending with "Page"
-  pages.forEach(function(page) {
-      page.style.display = 'none';
+  console.log("showing page:" + pageId);
+  document.getElementById('navigation').style.display = 'block';
+  var pages = document.querySelectorAll('[id$="Page"]');
+  pages.forEach(function (page) {
+    page.style.display = 'none';
   });
 
-  // Show the selected page
+  // Update selected page
   var selectedPage = document.getElementById(pageId);
   selectedPage.style.display = 'block';
 
   // Update the selected class for navigation buttons
   var buttons = document.querySelectorAll('#navigation button');
-  buttons.forEach(function(button) {
-      button.classList.remove('selected');
-      if (button.getAttribute('data-page') === pageId) {
-          button.classList.add('selected');
-      }
+  buttons.forEach(function (button) {
+    button.classList.remove('selected');
+    if (button.getAttribute('data-page') === pageId) {
+      button.classList.add('selected');
+    }
   });
 }
 
-// Function to run a script
 function runScript(scriptName) {
-  // Check if the user is authenticated before making API calls
-  if (!isAuthenticated()) {
-    showLoginPage();
-    return;
-  }
-
   // Update the apiUrl with the new API endpoint and IP address
-  var apiUrl = `https://bennettolsen.us:5000/${scriptName}`;
+  var apiUrl = `https://bennettolsen.us:5000/${scriptName}?password=${localStorage.getItem('password')}`;
 
   // Send a GET request to the updated API endpoint
   fetch(apiUrl)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error(`Error: ${response.status}`);
-          }
-          return response.text();
-      })
-      .then(result => {
-          console.log(result);
-      })
-      .catch(error => {
-          console.error(error);
-          alert(`Error executing ${scriptName} script: ${error.message}`);
-      });
-
-  // Toggle the "selected" class
-  toggleSelected(scriptName);
-}
-
-function toggleSelected(scriptName) {
-  // Get all buttons
-  var buttons = document.getElementsByTagName('button');
-
-  // Remove the "selected" class from all buttons
-  for (var i = 0; i < buttons.length; i++) {
-      buttons[i].classList.remove('selected');
-  }
-
-  // Add the "selected" class to the clicked button
-  var clickedButton = document.getElementById(`${scriptName}Button`);
-  clickedButton.classList.add('selected');
-}
-
-function isAuthenticated() {
-  // Check if the user is authenticated (you can implement your logic here)
-  // For example, you can use localStorage or sessionStorage to store authentication status
-  return localStorage.getItem('authenticated') === 'true';
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(result => {
+      console.log(result);
+    })
+    .catch(error => {
+      console.error(error);
+      alert(`Error executing ${scriptName} script: ${error.message}`);
+    });
 }
 
 function showLoginPage() {
   // Show the login page and hide other content
+  document.getElementById('navigation').style.display = 'none';
   var pages = document.querySelectorAll('[id$="Page"]');
-  pages.forEach(function(page) {
-      page.style.display = 'none';
+  pages.forEach(function (page) {
+    page.style.display = 'none';
   });
 
   var loginPage = document.getElementById('loginPage');
