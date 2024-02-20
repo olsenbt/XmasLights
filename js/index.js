d9946a5b914045bdcc3d6a19768eaee8713c2366
olsenbt72 <olsenbt@uw.edu>
2024-02-20 11:05:53 -0800

    fix spacing, fix pokemon random button

--- js/index.js
+++ js/index.js
@@ -16,7 +16,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Load Pokemon Page
   loadPokemonSprites();
-  document.getElementById('searchBar').addEventListener('input', filterPokemon);
+  let searchBar = document.getElementById('searchBar');
+  searchBar.addEventListener('input', filterPokemon);
+
+  let randomButton = document.getElementById('randomPokemon');
+
+  document.body.addEventListener('click', function (event) {
+    // Check if the click event target is not the clear button or its descendant
+    if (currentPage == 'pokemonPage' && event.target !== randomButton 
+    && !randomButton.contains(event.target)) {
+      if(!event.target.closest(".card")) {
+        filterPokemon();
+      }
+    }
+  });
+
+
+
 
   // Add event listeners to navigation buttons
   var buttons = document.querySelectorAll('.nav-item');
@@ -30,8 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
       // Set the clicked button to active
       this.classList.add('active');
 
-      var pageId = this.getAttribute('data-page');
-      showPage(pageId);
+      currentPage = this.getAttribute('data-page');
+      showPage(currentPage);
     });
   });
 
@@ -128,7 +144,7 @@ function showPage(pageId) {
     searchBar.style.display = 'none'; // Hide the searchBar
     randomPokemon.style.display = 'none'; // Hide the randomPokemon button
   }
-  
+
   var navbar = document.getElementById('navbar');
   navbar.style.display = (pageId === 'loginPage') ? 'none' : 'flex';
 }
@@ -137,7 +153,7 @@ function runScript(scriptName) {
   // Update the apiUrl with the new API endpoint and IP address
   let apiUrl = `https://bennettolsen.us:5000/run_script?password=${localStorage.getItem('password')}&script=${scriptName}`;
 
-  if(scriptName == "warm") {
+  if (scriptName == "warm") {
     apiUrl = `https://bennettolsen.us:5000/set_lights?password=${localStorage.getItem('password')}&r=224&g=233&b=40`;
   }
 
@@ -178,25 +194,25 @@ let selectedPokemonId = null; // Variable to store the currently selected Pokém
 
 // Function that loads all Pokémon sprites from JSON file and stores them in an object.
 async function loadPokemonSprites() {
-  const totalPokemon = 1025; 
+  const totalPokemon = 1025;
   const pokedex = document.getElementById("pokedex");
 
   const promises = [];
 
   for (let i = 1; i <= totalPokemon; i++) {
-      promises.push(getPokemonData(i));
+    promises.push(getPokemonData(i));
   }
 
   try {
-      const pokemon = await Promise.all(promises);
-      allPokemonData = pokemon;
-      pokemon.forEach((pokemon, i) => {
-          let pokedexEntry = genPokedexEntry(pokemon);
-          pokedexEntry.addEventListener('click', () => handlePokemonClick(pokemon.id));
-          pokedex.appendChild(pokedexEntry);
-      });
+    const pokemon = await Promise.all(promises);
+    allPokemonData = pokemon;
+    pokemon.forEach((pokemon, i) => {
+      let pokedexEntry = genPokedexEntry(pokemon);
+      pokedexEntry.addEventListener('click', () => handlePokemonClick(pokemon.id));
+      pokedex.appendChild(pokedexEntry);
+    });
   } catch (error) {
-      console.error('Error fetching Pokémon data:', error);
+    console.error('Error fetching Pokémon data:', error);
   }
 }
 
@@ -207,15 +223,15 @@ function filterPokemon() {
   const pokedex = document.getElementById('pokedex');
 
   while (pokedex.firstChild) {
-      pokedex.removeChild(pokedex.firstChild);
+    pokedex.removeChild(pokedex.firstChild);
   }
 
   const filteredPokemon = allPokemonData.filter(pokemon => pokemon.name.startsWith(searchTerm));
 
   filteredPokemon.forEach((pokemon, i) => {
-      let pokedexEntry = genPokedexEntry(pokemon);
-      pokedexEntry.addEventListener('click', () => handlePokemonClick(pokemon.id));
-      pokedex.appendChild(pokedexEntry);
+    let pokedexEntry = genPokedexEntry(pokemon);
+    pokedexEntry.addEventListener('click', () => handlePokemonClick(pokemon.id));
+    pokedex.appendChild(pokedexEntry);
   });
 }
 
@@ -230,7 +246,7 @@ async function getPokemonData(pokemonId) {
 function genPokedexEntry(pokemon) {
   let pokedexEntry = document.createElement('div'); // grid_item
   pokedexEntry.classList.add("grid_item");
-  pokedexEntry.setAttribute('id',pokemon.id);
+  pokedexEntry.setAttribute('id', pokemon.id);
 
   let card = document.createElement('div'); // card
   card.classList.add("card");
@@ -264,41 +280,41 @@ function handlePokemonClick(id) {
 
   // Read the color information from the pokemonColors.json file
   fetch('pokemonColors.json')
-      .then(response => response.json())
-      .then(pokemonColors => {
-          // Get the color information for the clicked Pokémon ID
-          const colors = pokemonColors[id].map(color => color.replace('#', ''));
+    .then(response => response.json())
+    .then(pokemonColors => {
+      // Get the color information for the clicked Pokémon ID
+      const colors = pokemonColors[id].map(color => color.replace('#', ''));
 
-          const apiUrl = `https://bennettolsen.us:5000/set_colors?password=candycane72&color1=${colors[0]}&color2=${colors[1]}&color3=${colors[2]}`;
-          console.log(apiUrl);
-          fetch(apiUrl)
-              .then(apiResponse => {
-                  if (!apiResponse.ok) {
-                      throw new Error(`API request failed with status: ${apiResponse.status}`);
-                  }
-                  return apiResponse.text();
-              })
-              .then(responseText => {
-                  console.log(responseText); // Log the response from the server
-              })
-              .catch(error => {
-                  console.error('Error making API request:', error);
-              });
+      const apiUrl = `https://bennettolsen.us:5000/set_colors?password=candycane72&color1=${colors[0]}&color2=${colors[1]}&color3=${colors[2]}`;
+      console.log(apiUrl);
+      fetch(apiUrl)
+        .then(apiResponse => {
+          if (!apiResponse.ok) {
+            throw new Error(`API request failed with status: ${apiResponse.status}`);
+          }
+          return apiResponse.text();
+        })
+        .then(responseText => {
+          console.log(responseText); // Log the response from the server
+        })
+        .catch(error => {
+          console.error('Error making API request:', error);
+        });
 
-          // Highlight the clicked Pokémon button
-          highlightSelectedPokemon(id);
-      })
-      .catch(error => {
-          console.error('Error reading pokemonColors.json:', error);
-      });
+      // Highlight the clicked Pokémon button
+      highlightSelectedPokemon(id);
+    })
+    .catch(error => {
+      console.error('Error reading pokemonColors.json:', error);
+    });
 }
 
 function chooseRandomPokemon() {
   const pokedex = document.getElementById('pokedex');
-  
+
   // Clear existing Pokémon entries
   while (pokedex.firstChild) {
-      pokedex.removeChild(pokedex.firstChild);
+    pokedex.removeChild(pokedex.firstChild);
   }
 
   // Choose a random Pokémon from the original data
@@ -309,9 +325,11 @@ function chooseRandomPokemon() {
   let pokedexEntry = genPokedexEntry(randomPokemon);
   pokedexEntry.addEventListener('click', () => handlePokemonClick(randomPokemon.id));
   pokedex.appendChild(pokedexEntry);
-  
+
   // Scroll to the selected Pokémon
-  pokedexEntry.scrollIntoView({ behavior: 'smooth' });
+  pokedexEntry.scrollIntoView({
+    behavior: 'smooth'
+  });
 
   // Update the search bar value
   document.getElementById('searchBar').value = ''; // Clear the search bar
@@ -323,16 +341,16 @@ function chooseRandomPokemon() {
 function highlightSelectedPokemon(pokemonId) {
   // Remove the highlight from the previously selected Pokémon button
   if (selectedPokemonId !== null) {
-      const prevSelectedButton = document.getElementById(selectedPokemonId);
-      if (prevSelectedButton) {
-          prevSelectedButton.style.backgroundColor = ''; // Remove the background color
-      }
+    const prevSelectedButton = document.getElementById(selectedPokemonId);
+    if (prevSelectedButton) {
+      prevSelectedButton.style.backgroundColor = ''; // Remove the background color
+    }
   }
 
   // Highlight the current selected Pokémon button
   const selectedButton = document.getElementById(pokemonId);
   if (selectedButton) {
-      selectedButton.style.backgroundColor = '#ffcccb'; // Set the background color as desired
-      selectedPokemonId = pokemonId; // Update the selected Pokémon ID
+    selectedButton.style.backgroundColor = '#ffcccb'; // Set the background color as desired
+    selectedPokemonId = pokemonId; // Update the selected Pokémon ID
   }
 }
